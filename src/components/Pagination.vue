857d264bed13125279c635a644ea4f60e8211ad0
Persinus <nguyenmanh2004devgame@gmail.com>
2025-07-11 17:20:37 +0700

    Thêm component phân trang Pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex justify-center items-center gap-2 mt-6">
+    <button
+      class="px-3 py-1 rounded bg-gray-700 text-white hover:bg-yellow-500"
+      :disabled="currentPage === 1"
+      @click="$emit('update:page', currentPage - 1)"
+    >
+      &lt;
+    </button>
+    <span class="font-semibold text-yellow-400"
+      >Trang {{ currentPage }} / {{ totalPages }}</span
+    >
+    <button
+      class="px-3 py-1 rounded bg-gray-700 text-white hover:bg-yellow-500"
+      :disabled="currentPage === totalPages"
+      @click="$emit('update:page', currentPage + 1)"
+    >
+      &gt;
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  currentPage: Number,
+  totalPages: Number,
+});
+defineEmits(["update:page"]);
+</script>
