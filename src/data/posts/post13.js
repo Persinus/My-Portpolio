8ad264d40f0f837a652cd6ab3ecfc48af68f3a28
Persinus <nguyenmanh2004devgame@gmail.com>
2025-07-11 18:25:40 +0700

    📝 Thêm bài viết post13

---
+++ src/data/posts/post13.js
@@ -0,0 +1,73 @@
+export default {
+  id: 13,
+  title: '🎁 14 Unity Asset Cực Xịn Cho Dev Game (Phần 3)',
+  cover: 'https://i.pinimg.com/1200x/4a/ee/ed/4aeeed5ba09f9e3679cd126e70f506fa.jpg',
+  author: 'Nguyễn Văn Mạnh',
+  authorAvatar: 'https://avatars.githubusercontent.com/u/199482290?v=4',
+  date: '11/07/2025 11:17 AM',
+  topic: 'Unity Asset, Game Dev',
+  toc: [
+    'Giới thiệu',
+    'Danh sách asset',
+    'Chi tiết từng gói',
+    'Hướng dẫn import',
+    'Lưu ý',
+    'Tải xuống',
+    'Liên hệ'
+  ],
+  warning: '',
+  barge: ['Unity', 'Asset', 'GameDev', 'Free', 'UnityPackage'],
+  content: `
+<p>Phần cuối gồm các asset liên quan đến UI, nhân vật hoạt hình, quái vật và đạn dược phong cách toon – rất phù hợp cho game prototype nhanh.</p>
+
+<h2>9️⃣ Modern Shooting UI Pack v1.1</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/33653222-f162-490a-94ed-e77f6a23af96.webp" width="100%"/></td>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/97d14835-cbf4-4235-b01a-9d1a6562d783.webp" width="100%"/></td>
+</tr></table>
+<p>📱 UI cho game FPS hiện đại, dễ dùng</p>
+<p>🔗 <a href="https://www.mediafire.com/file/ly9qaatz6rawrbl/Modern_Shooting_UI_Pack_v1.1_%25281%2529.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+<h2>🔟 Simple Military - Cartoon War v1.35</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/7a985edc-ad9b-4d80-bfae-b4f0be121259.webp" width="100%"/></td>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/5c9df370-e303-40f3-8cd4-f9ea8754ce9c.webp" width="100%"/></td>
+</tr></table>
+<p>👮‍♂️ Bộ binh lính hoạt hình đơn giản</p>
+<p>🔗 <a href="https://www.mediafire.com/file/6dsmtvnnord2pgc/Simple_Military_-_Cartoon_War_v1.35.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+<h2>1️⃣1️⃣ Toon Soldiers - Armies v1.0</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/9893fd3f-2e4b-40ff-bdbd-5ec53e1d6d48.webp" width="100%"/></td>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/98a461bd-dbf3-4203-959c-d80609d59761.webp" width="100%"/></td>
+</tr></table>
+<p>🎖 Chi tiết cao, animation sẵn sàng</p>
+<p>🔗 <a href="https://www.mediafire.com/file/i8ll0mkr5co6201/Toon_Soldiers_-_Armies_v1.0.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+<h2>1️⃣2️⃣ Toon Zombies - Extended Pack v1.0</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/37d886a5-9322-4eab-9532-0ef056631c8e.webp" width="100%"/></td>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/37d886a5-9322-4eab-9532-0ef056631c8e.webp" width="100%"/></td>
+</tr></table>
+<p>🧟‍♂️ Bộ zombie toon mở rộng – hoạt hình đầy đủ</p>
+<p>🔗 <a href="https://www.mediafire.com/file/ziu2sw03i2703yz/Toon_Zombies_-_Extended_Pack_1.0.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+<h2>1️⃣3️⃣ Animated 2D Monsters (Fantazia) v2.01</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/53c990f1-d500-4233-89ea-9fab7ceee2d9.webp" width="100%"/></td>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/c184c913-85f4-4c05-a53c-f5600e948d48.webp" width="100%"/></td>
+</tr></table>
+<p>👹 Quái vật 2D đầy đủ animation cho game fantasy</p>
+<p>🔗 <a href="https://www.mediafire.com/file/5grukxgqdicxe07/Animated_2D_Monsters_%2528Fantazia%2529_v2.01.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+<h2>1️⃣4️⃣ Toon Projectiles 2 v1.0</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/eff4e06e-8ce7-47d3-a38e-28b7d60ef006.webp" width="100%"/></td>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/f27f908e-3865-401d-b914-99f9a40bfcde.webp" width="100%"/></td>
+</tr></table>
+<p>💥 Đạn hoạt hình đẹp mắt, dùng cho soldier/zombie</p>
+<p>🔗 <a href="https://www.mediafire.com/file/c6b6revg5biara6/Toon_Projectiles_2_v1.0.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+`,
+  code: []
+}
