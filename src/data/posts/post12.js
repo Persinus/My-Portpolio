31704b0aa7f617ea4e31eff2fc1c97ee1decbfe4
Persinus <nguyenmanh2004devgame@gmail.com>
2025-07-11 18:25:29 +0700

    📝 Thêm bài viết post12

---
+++ src/data/posts/post12.js
@@ -0,0 +1,56 @@
+export default {
+  id: 12,
+  title: '🎁 14 Unity Asset Cực Xịn Cho Dev Game (Phần 2)',
+  cover: 'https://i.pinimg.com/1200x/ef/2e/a2/ef2ea2a41db796474fb4c5bbd2cd1fa7.jpg',
+  author: 'Nguyễn Văn Mạnh',
+  authorAvatar: 'https://avatars.githubusercontent.com/u/199482290?v=4',
+  date: '11/07/2025 11:17 AM',
+  topic: 'Unity Asset, Game Dev',
+  toc: [
+    'Giới thiệu',
+    'Danh sách asset',
+    'Chi tiết từng gói',
+    'Hướng dẫn import',
+    'Lưu ý',
+    'Tải xuống',
+    'Liên hệ'
+  ],
+  warning: '',
+  barge: ['Unity', 'Asset', 'GameDev', 'Free', 'UnityPackage'],
+  content: `
+<p>Tiếp tục series asset Unity cực chất – phần 2 tập trung vào animation, tween, debug và live coding.</p>
+
+<h2>5️⃣ Animation Designer v1.2.4</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/da3d43d1-c9fd-4b2b-aaa6-fa42d2cd8dda.webp" width="100%"/></td>
+<td><img src="https://github.com/user-attachments/assets/f0f847ff-f0d9-4e02-9160-5deeb5d7052a" width="100%"/></td>
+</tr></table>
+<p>🕺 Tạo animation mượt, trực quan trong Editor</p>
+<p>🔗 <a href="https://www.mediafire.com/file/3qzawff4rbehzf0/Animation_Designer_v1.2.4.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+<h2>6️⃣ DOTween Pro v1.0.381</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/9488fcf5-a4fd-4876-a114-f59349607009.webp" width="100%"/></td>
+<td><img src="https://i.imgur.com/3UOpvff.png" width="100%"/></td>
+</tr></table>
+<p>🎬 Tween animation chuyển cảnh chuyên nghiệp</p>
+<p>🔗 <a href="https://www.mediafire.com/file/p3bes5ynf9wn9pj/DOTween_Pro_v1.0.381_%2528Feb_4_2024%2529.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+<h2>7️⃣ Editor Console Pro v3.977</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/3f5c18b8-037b-44f1-ada0-9a00f72ebac7.webp" width="100%"/></td>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/c2bf7661-e545-4458-805e-cc87c0ace1b4.webp" width="100%"/></td>
+</tr></table>
+<p>🛠 Debug hiệu quả hơn với console nâng cao</p>
+<p>🔗 <a href="https://www.mediafire.com/file/d4tajc4qo7gqln6/Editor_Console_Pro_v3.977_%252813_Nov_2024%2529.unitypackage/file" target="_blank">Tải tại đây</a></p>
+
+<h2>8️⃣ Hot Reload Edit Code v1.12.9</h2>
+<table><tr>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/key-image/0e70e48a-0d08-4804-ad0e-1659ca515687.webp" width="100%"/></td>
+<td><img src="https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/c8475eb7-222d-40b1-badf-adb4bc935bb5.webp" width="100%"/></td>
+</tr></table>
+<p>💻 Sửa code và xem kết quả ngay trong Editor</p>
+<p>🔗 <a href="https://www.mediafire.com/file/1y5z0nn9zmo0ifa/Hot_Reload_Edit_Code_Without_Compiling_v1.12.9.unitypackage/file" target="_blank">Tải tại đây</a></p>
+`,
+  code: []
+}
