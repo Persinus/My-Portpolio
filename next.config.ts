719907dd679610805b82a07642421ea0187c0783
mạnh nguyễn <procnttdzaivl@gmail.com>
2025-08-31 07:20:25 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'httpsd',
+        protocol: 'https',
         hostname: 'cdn.jsdelivr.net',
         port: '',
         pathname: '/**',
